1ab0ac2a4967e0d547f7ba5b689c98ed0b2b31fc
aidendumas <89714869+aidendumas@users.noreply.github.com>
2021-09-28 19:36:01 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-//import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 //import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
