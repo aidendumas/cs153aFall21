9a551379fe90811bf64e736c0276d253a7e532ba
aidendumas <89714869+aidendumas@users.noreply.github.com>
2021-09-28 20:26:20 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,12 +4,56 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  return (
+      <View style={{ flexDirection: 'row',
+                     margin:"25px",
+                     border:"thick solid black",
+                     padding:'10px',
+                     justifyContent: 'space-around', }}>
+
+        <Text> home screen </Text>
+
+        <Button
+          title="Go to profile"
+          onPress={() =>
+            navigation.navigate('Profile', { name: 'profile', greeting:'Hi!' })
+               // we're passing a parameter name:'Jane' to the Profile component!
+          }
+        />
+    </View>
+  );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  return (
+    <Text style={styles.textStyle}>
+      profile
+    </Text>
+  );
+};
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>test123</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MyStack/>
   );
 }
 
@@ -19,5 +63,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize:60,
+    color:'red',
+
   },
 });
