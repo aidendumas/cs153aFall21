350a2fa63aedefc0038193fa5226228ac792cc9b
aidendumas <89714869+aidendumas@users.noreply.github.com>
2021-09-28 20:07:07 -0400

    updates

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-//import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <Text>test123</Text>
       <StatusBar style="auto" />
     </View>
   );
