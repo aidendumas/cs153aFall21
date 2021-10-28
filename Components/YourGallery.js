bc8ab79624392e1e16b4d8bc1cbe92858bac5028
aidendumas <89714869+aidendumas@users.noreply.github.com>
2021-10-28 12:12:00 -0400

    Update YourGallery.js

--- Components/YourGallery.js
+++ Components/YourGallery.js
@@ -5,7 +5,9 @@ import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 const YourGallery = ({ navigation, route }) => {
   return (
     <View>
-      Testing
+      <Text>
+        Testing
+      </Text>
     </View>
   );
 }
